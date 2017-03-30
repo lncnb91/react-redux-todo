6d3d949da7620727ad81aa9abf1115cd35412864
Truong Tuan Quang <tuanquanghpvn@gmail.com>
2017-03-30 16:32:40 +0700

    TODO

---
+++ src/routes/Login/components/Login.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+// export const Login = (props) => (
+//   <div>
+//     <form>
+//       <label>Username</label>
+//       <input type="text" name="username" />
+//       <br/>
+//       <label>Password</label>
+//       <input type="password" name="password" />
+//       <br/>
+//       <button>Submit</button>
+//     </form>
+//   </div>
+// )
+//
+// Login.propTypes = {
+//   username     : React.PropTypes.string.isRequired,
+//        : React.PropTypes.string.isRequired
+// }
+//
+// export default Counter
+
+class Login extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  onChangeUsername = (e) => {
+    this.setState({ username: e.target.value })
+  }
+  onChangePassword = (e) => {
+    this.setState({ password: e.target.value })
+  }
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.login(this.state)
+    return false
+  }
+
+  render () {
+    const { username, password } = this.state
+    const { message } = this.props
+    return (
+      <div>
+        {message ? <div>{message}</div> : null}
+        <form onSubmit={this.onSubmit}>
+          <label>Username</label>
+          <input type='text' name='username' value={username} onChange={this.onChangeUsername} />
+          <br />
+          <label>Password</label>
+          <input type='password' name='password' value={password} onChange={this.onChangePassword} />
+          <br />
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
---
+++ src/routes/Login/modules/login.js
@@ -0,0 +1,62 @@
+// ------------------------------------
+// Constants
+// ------------------------------------
+const LOGIN_REQUEST = 'LOGIN_REQUEST'
+const LOGIN_REQUEST_SUCCESS = 'LOGIN_REQUEST_SUCCESS'
+const LOGIN_REQUEST_ERROR = 'LOGIN_REQUEST_ERROR'
+// ------------------------------------
+// Actions
+// ------------------------------------
+export function login ({ username, password }) {
+  return (dispatch, getState) => {
+    dispatch({
+      type    : LOGIN_REQUEST,
+      payload : { username, password, loading: true }
+    })
+     // todo: call api login
+    return new Promise((resolve, reject) => {
+      resolve() // test success
+      // reject(new Error('Looix'))
+    })
+      .then(rs => {
+        dispatch(loginSuccess())
+      })
+      .catch(err => dispatch(loginError(err)))
+  }
+}
+
+export function loginSuccess () {
+  return {
+    type    : LOGIN_REQUEST_SUCCESS,
+    payload : { message: 'Login success' }
+  }
+}
+export function loginError (err) {
+  return {
+    type    : LOGIN_REQUEST_ERROR,
+    payload : { message: err.message }
+  }
+}
+
+export const actions = {
+  login,
+  loginSuccess,
+  loginError
+}
+// ------------------------------------
+// Action Handlers
+// ------------------------------------
+const ACTION_HANDLERS = {
+  [LOGIN_REQUEST]    : (state, action) => ({ ...state, ...action.payload }),
+  [LOGIN_REQUEST_SUCCESS]    : (state, action) => ({ ...state, ...action.payload }),
+  [LOGIN_REQUEST_ERROR] : (state, action) => ({ ...state, ...action.payload })
+}
+// ------------------------------------
+// Reducer
+// ------------------------------------
+const initialState = 0
+export default function loginReducer (state = initialState, action) {
+  const handler = ACTION_HANDLERS[action.type]
+
+  return handler ? handler(state, action) : state
+}
